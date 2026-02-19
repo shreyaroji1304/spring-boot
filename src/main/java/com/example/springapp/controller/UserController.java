3380efa890a57278e4f0087474e0c1b36fdda382
Shreya Roji <shreyaroji1304@gmail.com>
2026-02-19 22:11:15 +0530

    Fixed YAML schema

--- src/main/java/com/example/springapp/controller/UserController.java
+++ src/main/java/com/example/springapp/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.example.springapp.controller;
 
-import com.example.springapp.model.User;
-import com.example.springapp.service.UserService;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.example.springapp.model.User;
+import com.example.springapp.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
@@ -55,7 +64,7 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("/health")
+    @GetMapping("/showhealth")
     public ResponseEntity<String> health() {
         return new ResponseEntity<>("API is running", HttpStatus.OK);
     }
